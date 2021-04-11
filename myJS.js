a2f4f8ee60b27312f493be4a657029ea6a167483
Lars Brevén <lars.breven@telia.com>
2021-04-11 17:29:15 +0200

    Changes in definition of boxes

--- myJS.js
+++ myJS.js
@@ -8,33 +8,22 @@ let playerPos = {horizontalPos:0,verticalPos:0};
 function createGameBoard() {
     for (let verticalPos = 0; verticalPos < tileMap01.height; ++verticalPos) {
         for (let horizontalPos = 0; horizontalPos < tileMap01.width; ++horizontalPos) {
-        makeTheBoxes(tileMap01.mapGrid[verticalPos][horizontalPos][0],verticalPos,horizontalPos);
+            makeTheBoxes(tileMap01.mapGrid[verticalPos][horizontalPos][0],verticalPos,horizontalPos);
+        }
     }
-  }
 }
 
 function makeTheBoxes(boxType,verticalPos,horizontalPos) {
-  let newBox = document.createElement("div");
-  newBox.classList.add("box");
-  newBox.id = "verticalPos"+ verticalPos + "horizontalPos" + horizontalPos;
+    let newBox = document.createElement("div");
+    newBox.classList.add("box");
+    newBox.id = "verticalPos"+ verticalPos + "horizontalPos" + horizontalPos;
  
-  switch (boxType) {
-    case "W":                           // Wall (W = grid from tileMap01)
-      newBox.classList.add("wall");
-      break;
-    case "B":                           // Movable block (B = grid from tileMap01)
-      newBox.classList.add("movableBlock");
-      break;
-    case "P":                           // Player (P = grid from tileMap01)
-      newBox.classList.add("player");
-      break;
-    case "G":                           // Goal area (G = grid from tileMap01)
-      newBox.classList.add("goalArea");
-      break;
-    default:
-  }
-
-  gameBoard.appendChild(newBox);
+    if (boxType === "W") newBox.classList.add("wall");
+    if (boxType === "B") newBox.classList.add("movableBlock");
+    if (boxType === "P") newBox.classList.add("player");
+    if (boxType === "G") newBox.classList.add("goalArea");
+    
+    gameBoard.appendChild(newBox);
 }
 
 // --------------------- Run code lines --------------------------------------
