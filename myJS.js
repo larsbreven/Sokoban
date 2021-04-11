d3bf1bfc6365633efe39182151659ce28fa399be
Lars Brevén <lars.breven@telia.com>
2021-04-11 15:16:25 +0200

    Images and game board included

--- myJS.js
+++ myJS.js
@@ -1,69 +1,45 @@
-//------ global varibles ----------------------------------------------------------------------------------
-const myBoard = document.getElementById("board");
-const boardWidth = 600;
-const boardHeight = 300;
-const luckyNumber = 5;
-var idCounter = 0;
+// --------------------------  Global variables ------------------------------
 
-//------ functions ----------------------------------------------------------------------------------------
+const gameBoard = document.getElementById("board");
+let playerPos = {horizontalPos:0,verticalPos:0};
 
-function initBoard() {
-    var blackBoxBool = true;
+// ------------------------------ Functions ----------------------------------
 
-    for (let boxTotalHeight = 0; boxTotalHeight < boardHeight; boxTotalHeight = boxTotalHeight + 50) {
-        for (let boxTotalWidth = 0; boxTotalWidth < boardWidth; boxTotalWidth = boxTotalWidth + 50) {
-
-            if (blackBoxBool) {
-                makeBoxOnBoard("black");
-            }
-            else {
-                makeBoxOnBoard("white");
-            }
-
-            blackBoxBool = !blackBoxBool;
-        }
-
-        blackBoxBool = !blackBoxBool;
+function createGameBoard() {
+    for (let verticalPos = 0; verticalPos < tileMap01.height; ++verticalPos) {
+        for (let horizontalPos = 0; horizontalPos < tileMap01.width; ++horizontalPos) {
+        makeTheBoxes(tileMap01.mapGrid[verticalPos][horizontalPos][0],verticalPos,horizontalPos);
     }
+  }
 }
 
-function makeBoxOnBoard(colorOfBox) {
-    var newBox = document.createElement("div");
+function makeTheBoxes(boxType,verticalPos,horizontalPos) {
+  let newBox = document.createElement("div");
+  newBox.classList.add("box");
+  newBox.id= "verticalPos"+ verticalPos + "horizontalPos" + horizontalPos;
 
-    newBox.id = ++idCounter;
+  switch (boxType) {
+    case "W":                           // Wall (W = grid from tileMap01)
+      newBox.classList.add("wall");
+      break;
+    case "B":                           // Movable block (B = grid from tileMap01)
+      newBox.classList.add("movableBlock");
+      break;
+    case "P":                           // Player (P = grid from tileMap01)
+      playerPos.horizontalPos = horizontalPos;
+      playerPos.verticalPos = verticalPos; 
+      newBox.classList.add("player");
+      break;
+    case "G":                           // Goal area (G = grid from tileMap01)
+      newBox.classList.add("goalArea");
+      break;
+    default:
+  }
 
-    newBox.classList.add("box");
-    newBox.classList.add(colorOfBox + "Box");
-
-    myBoard.appendChild(newBox);
-
-    if (idCounter === luckyNumber) {
-        newBox.classList.add("player");
-    }
+  gameBoard.appendChild(newBox);
 }
 
-function move(offsetPos) {
-    var playerElement = document.getElementsByClassName("player")[0];
-    var playerNextElementId = Number(playerElement.id) + Number(offsetPos);
-    var playerNextBox = document.getElementById(playerNextElementId);
-    console.log(playerNextBox);
-    
-    if (playerNextElementId !== 0 && playerNextElementId <= idCounter) {
-        playerElement.classList.toggle("player");
-        playerNextBox.classList.toggle("player");
-    }
+// --------------------- Run code lines --------------------------------------
+document.getElementsByTagName("body")[0].style.backgroundColor = "lightgrey";
 
-
-}
-
-
-
-
-
-
-//------ run code lines -----------------------------------------------------------------------------------
-
-document.getElementsByTagName("body")[0].style.backgroundColor = "gray";
-
-initBoard();
-//makeBoxOnBoard("blue"); test passed
+createGameBoard();
